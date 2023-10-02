4502f6e98bf748c624214ab930a87a8bca5404ed
m-amaris <66205563+m-amaris@users.noreply.github.com>
2023-10-02 19:36:03 +0200

    Create practica0.ino

---
+++ practica0.ino/practica0.ino
@@ -0,0 +1,9 @@
+void setup() {
+  Serial.begin(115200); // Inicializar la consola
+  Serial.println("Hola mundo!"); // Mostrar texto en la consola
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
