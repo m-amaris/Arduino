d59434b69810047856e4dd064c584c696982e6bb
m-amaris <66205563+m-amaris@users.noreply.github.com>
2023-10-06 20:00:33 +0200

    Plantilla Basica P0 Multiactuador

--- P0.Multiactuador/P0.Multiactuador.ino
+++ P0.Multiactuador/P0.Multiactuador.ino
@@ -5,38 +5,46 @@ El actuador de enchufe tiene conectado como carga un led blanco, y para simular 
 El actuador de interruptor tiene conectado como carga un led rojo, que es activado por un relé. Mediante un se activa/desactiva la carga.
 */
 #include XXXXX
-/* Definición de constantes COMUNES */ #define ENCENDER HIGH
+
+/* Definición de constantes COMUNES */ 
+#define ENCENDER HIGH
 #define ENCENDIDO true
+
 /* Definición de constantes y variables del ACTUADOR DE ENCHUFE */
 const int pin_carga_actuador_enchufe = 11;
 #define APAGADO_ENCHUFE LOW
-bool estado_carga_actuador_enchufe=APAGADO;
-Hogar Digital
-* Definición de constantes y variables del ACTUADOR DE INTERRUPTOR*/
+bool estado_carga_actuador_enchufe = APAGADO;
+
+/* Definición de constantes y variables del ACTUADOR DE INTERRUPTOR*/
 const int pin_carga_actuador_interruptor = 12;
 const int pin_rele actuador_interruptor = 13;
 const int pin_pulsador_interruptor = 15;
-bool estado_carga_actuador_enchufe=APAGADO;
+bool estado_carga_actuador_enchufe = APAGADO;
+
 void setup() {
-Serial. begin(115200);
-Serial.printIn("");
-Serial. printIn("Arrancando");
-setup_actuador_enchufe();
-setup_actuador_interruptor();
-Serial.printIn("Inicialización del dispositivo terminada.");
+  Serial.begin(115200);
+  Serial.printIn("");
+  Serial.printIn("Arrancando");
+  setup_actuador_enchufe();
+  setup_actuador_interruptor();
+  Serial.printIn("Inicialización del dispositivo terminada.");
 }
+
 void setup_actuador_enchufe() {
-Serial.print("Inicializando el actuador de enchufe ");
-Serial. printIn("hecho.");
-｝
+  Serial.print("Inicializando el actuador de enchufe ");
+  Serial.printIn("hecho.");
+}
+
 void setup_actuador_interruptor() {
-serial.print("Inicializando el actuador de interruptor ");
-Serial.printIn("hecho.");
+  serial.print("Inicializando el actuador de interruptor ");
+  Serial.printIn("hecho.");
 }
-void 100p() {
-Il Código común a ambos actuadores
-1oop_actuador_enchufe();
-1oop_actuador_ interruptor();
+
+void loop() {
+  // Código común a ambos actuadores 1oop_actuador_enchufe();
+  loop_actuador_interruptor();
+  loop_actuador_enchufe();
 }
+
 void loop_actuador_enchufe() {}
 void Loop_actuador_interruptor() {}
